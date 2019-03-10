987b88bce0c2264164bad2490e6b7df5098be124
zugcoin <39747078+zugcoin@users.noreply.github.com>
2019-03-10 15:21:23 +0100

    Ball

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -3,14 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Snake : MonoBehaviour {
+    public Transform prefabBody;
 
+    int typeMvt = 0;
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
+
+    void InitBody() { }
+
+    void InitTrail() { }
+
+    void CreateBody()
+    {
+
+    }
+
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        //turn left
+        if (Input.GetKeyDown(KeyCode.Q)) {
+            typeMvt = -1;
+        }
+
+        //Turn right
+        if (Input.GetKeyDown(KeyCode.W)) {
+            typeMvt = 1;
+        }
+        Movement();
+    }
+
+    void Movement()
+    {
+        //Snake movement
+        transform.position = transform.position + transform.forward * 0.1f;
+    }
 }
