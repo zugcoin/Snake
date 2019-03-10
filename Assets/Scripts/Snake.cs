51ec99d6c1c1e4ad946ba98229a937643610b4ad
zugcoin <39747078+zugcoin@users.noreply.github.com>
2019-03-10 15:28:08 +0100

    Ball

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -38,6 +38,29 @@ public class Snake : MonoBehaviour {
     {
         //Snake movement
         transform.position = transform.position + transform.forward * 0.1f;
+
+        //Player wants to rotate the Snake
+        if (typeMvt != 0)
+        {
+
+            //Only rotate every unit
+            if (System.Math.Round(transform.position.z, 2) % 1 == 0 && System.Math.Round(transform.position.x, 2) % 1 == 0)
+            {
+                if (typeMvt != 0)
+                {
+                    switch (typeMvt)
+                    {
+                        case -1:
+                            transform.RotateAround(transform.position, Vector3.up, -90);
+                            break;
+                        case 1:
+                            transform.RotateAround(transform.position, Vector3.up, 90);
+                            break;
+                    }
+                    typeMvt = 0;
+                }
+            }
+        }
     }
 
 
